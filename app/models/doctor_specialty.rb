9f574ab1e094ac6eb5c958828ff0b5bb2d17285e
Creiwry <jay.gardos@outlook.com>
2022-10-25 19:45:47 +0200

    tables and models: Patients, Doctors, Cities, Appointments, Doctors, Doctor_Specialties, Specialties;
    migration

---
+++ app/models/doctor_specialty.rb
@@ -0,0 +1,6 @@
+class DoctorSpecialty < ApplicationRecord
+  belongs_to :doctor
+  belongs_to :specialty
+ 
+
+end
--- db/migrate/20221025155020_create_appointments.rb
+++ db/migrate/20221025155020_create_appointments.rb
@@ -3,8 +3,8 @@ class CreateAppointments < ActiveRecord::Migration[7.0]
     create_table :appointments do |t|
       t.belongs_to :doctor, index: true
       t.belongs_to :patient, index: true
+      t.belongs_to :city, index: true
       t.datetime :date
-
       t.timestamps
     end
   end
