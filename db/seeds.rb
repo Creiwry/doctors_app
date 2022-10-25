4f0710aabc7e03599a12e161cfafdfac86f4d54f
Creiwry <jay.gardos@outlook.com>
2022-10-25 18:51:28 +0200

    ibid, seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+specialty =["Dermatology","Diagnostic radiology","Family medicine","Internal medicine","Neurology","Obstetrics and gynecology","Ophthalmology","Pediatrics","Psychiatry","Radiation oncology","Urology"]
+
+
+
+100.times do |a|
+  a = Doctor.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, specialty: specialty.sample, zip_code: Faker::Address.zip_code)
+end
+
+100.times do |b|
+  b = Patient.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+end
+
+doctors = Doctor.all
+patients = Patient.all
+
+100.times do |c|
+  c = Appointment.create(doctor: doctors.sample, patient: patients.sample, date: Faker::Date.in_date_period)
+end
